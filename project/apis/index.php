280beb1e702475b669fde7e12e62de9d3371721e
Amir Aslan Aslan <amiraslanaslani6@gmail.com>
2025-06-22 13:45:34 +0330

    Encapsulate application logic in a main function in index.php to improve code organization and readability.

--- project/apis/index.php
+++ project/apis/index.php
@@ -6,6 +6,10 @@ use Aslan\Chat\DB as DB;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$db = new DB();
-$app = new App($db);
-$app->run();
+function main() {
+    $db = new DB();
+    $app = new App($db);
+    $app->run();
+}
+
+main();
